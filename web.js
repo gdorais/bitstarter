311599967467dc6f4716e029988c3cf34eb9b2b7
Greg Dorais <gadorais@gmail>
2013-07-04 08:49:29 -0700

    try using http.createServer

--- web.js
+++ web.js
@@ -1,15 +1,23 @@
-var express = require('express');
-var app = express.createServer(express.logger());
+// var express = require('express');
+// var app = express.createServer(express.logger());
+var http = require('http');
+var fs = require('fs');
 var index = fs.readFileSync('index.html');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(index);
+}).listen(9615);
 
 // app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
 // });
-app.get('/', function(request, response) {
-  response.send(index);
-});
+//app.get('/', function(request, response) {
+//  response.send(index);
+//});
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+//var port = process.env.PORT || 5000;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
+
