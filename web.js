7efb272b7fc9fa50d33210d4754aa9c0ce60dbae
Greg Dorais <gadorais@gmail>
2013-07-04 22:17:11 -0700

    read and output index.html

--- web.js
+++ web.js
@@ -1,23 +1,14 @@
-// var express = require('express');
-// var app = express.createServer(express.logger());
-var http = require('http');
+var express = require('express');
+var app = express.createServer(express.logger());
 var fs = require('fs');
-var index = fs.readFileSync('index.html');
+var text_buf = fs.readFileSync('index.html');
+var text = text_buf.toString();
+app.get('/', function(request, response) {
+ response.send(text);
+});
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(index);
-}).listen(9615);
-
-// app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
-// });
-//app.get('/', function(request, response) {
-//  response.send(index);
-//});
-
-//var port = process.env.PORT || 5000;
-//app.listen(port, function() {
-//  console.log("Listening on " + port);
-//});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 
